d5c298303514a6df95beae42f29142a18228e532
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-02-17 11:51:36 -0500

    added solution for debugger

--- edit-this-script.js
+++ edit-this-script.js
@@ -3,10 +3,13 @@
   var decode = eval(atob(e));
   question = decode("V2hhdCBkb2VzIGEgY2xvdWQgd2VhciBvbiBpdHMgYnV0dD8=");
   answer = decode("VGh1bmRlcnBhbnRzLg==");
+  debugger;
   question = decode("V2hhdCBkbyB5b3UgY2FsbCBhIGNvdyB3aXRoIG5vIGxlZ3M/");
   answer = decode("R3JvdW5kIGJlZWYu");
+  debugger;
   (function(){
     question = decode("V2h5IHdhcyB0aGUgbGl0dGxlIGluayBkcm9wIGNyeWluZz8=");
     answer = decode("SGlzIGZhdGhlciB3YXMgaW4gdGhlIHBlbiBhbmQgaGUgZGlkbid0IGtub3cgaG93IGxvbmcgdGhlIHNlbnRlbmNlIHdvdWxkIGJlLg==");
+    debugger;
   })();
 })();
